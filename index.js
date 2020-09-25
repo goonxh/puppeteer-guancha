b2ab542215d0480fbc159230de8f92b98d1caf8c
goonxh <xh011234@163.com>
2020-09-25 16:43:11 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const puppeteer = require("puppeteer");
+// const iPhone = puppeteer.devices["iPhone XR"];
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true
+    headless: true,
   });
   const page = await browser.newPage();
+  // await page.emulate(iPhone);
+  await page.setViewport({width: 420, height: 1440});
+  await page.goto("https://m.weibo.cn/u/2167365780");
+  await page.waitForTimeout(5000);
   await page.screenshot({
-    path: "c:/temp/baidu.png"
+    path: "./download/test.jpg",
   });
+  await browser.close();
 })();
